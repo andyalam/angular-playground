971b6db2fb902649b84d68f1cb3a029e96ce08a9
andyalam <andyalam@hotmail.com>
2017-04-02 14:26:12 -0700

    init auction app and component scaffolding, basic service

---
+++ auctionApp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { MaterialModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { SearchComponent } from './components/search/search.component';
+import { StarsComponent } from './components/stars/stars.component';
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    SearchComponent,
+    StarsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
