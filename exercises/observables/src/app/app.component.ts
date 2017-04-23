52ce8d15e03fc379853ed7f018de745e7f8946b9
andyalam <andyalam@hotmail.com>
2017-04-23 09:21:10 -0700

    completed observables

--- exercises/observables/src/app/app.component.ts
+++ exercises/observables/src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { UsersService } from './users.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  userActivatedObsSubscription: Subscription;
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.userActivatedObsSubscription = this.usersService.userActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1Activated = true;
+        } else if (id === 2) {
+          this.user2Activated = true;
+        }
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.userActivatedObsSubscription.unsubscribe();
+  }
+
 }
