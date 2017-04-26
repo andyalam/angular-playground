58781398d7faaf775d07b2cd06308865cc68cd6a
andyalam <andyalam@hotmail.com>
2017-04-25 20:35:09 -0700

    completed form assignment

--- exercises/forms-assignment/src/app/app.component.ts
+++ exercises/forms-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') emailForm: NgForm;
+  private subscriptions: string[] = ["Basic", "Advanced", "Pro"];
+  private defaultSubscription: string = this.subscriptions[1];
+  private submitted: boolean = false;
+  private data;
 
+  onSubmit() {
+    console.log(this.emailForm);
+    this.submitted = true;
+    this.data = {
+      email: this.emailForm.value.email,
+      subscription: this.emailForm.value.subscription,
+      password: this.emailForm.value.password
+    }
+  }
 }
