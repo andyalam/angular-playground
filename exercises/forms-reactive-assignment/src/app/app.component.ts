0319d4e5cbea70a148229afe754edb2c7499069a
andyalam <andyalam@hotmail.com>
2017-04-26 20:27:45 -0700

    working on reactive forms assignment

--- exercises/forms-reactive-assignment/src/app/app.component.ts
+++ exercises/forms-reactive-assignment/src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatuses: string[] = ['Stable', 'Critical', 'Finished'];
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'mail': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl(null)
+    });
+  }
+
+  forbiddenProject() {
+
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
 }
---
+++ exercises/forms-reactive-assignment/src/app/custom-validators.ts
@@ -0,0 +1,9 @@
+export default class CustomValidators {
+	static invalidProjectName() {
+
+	}
+
+	static asyncInvalidProjectName() {
+		
+	}
+}
