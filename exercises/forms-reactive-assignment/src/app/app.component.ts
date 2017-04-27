7d213c76966ddc2250ba7fe999fbbba2f7d61b47
andyalam <andyalam@hotmail.com>
2017-04-26 23:53:34 -0700

    finished reactive forms exercise

--- exercises/forms-reactive-assignment/src/app/app.component.ts
+++ exercises/forms-reactive-assignment/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { CustomValidators } from './custom-validators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,7 +14,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, Validators.required),
+      'projectName': new FormControl(
+        null,
+        [Validators.required, CustomValidators.invalidProjectName],
+        CustomValidators.asyncInvalidProjectName
+      ),
       'mail': new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus': new FormControl(null)
     });
--- exercises/forms-reactive-assignment/src/app/custom-validators.ts
+++ exercises/forms-reactive-assignment/src/app/custom-validators.ts
@@ -1,9 +1,25 @@
-export default class CustomValidators {
-	static invalidProjectName() {
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
+export class CustomValidators {
+	static invalidProjectName(control: FormControl): {[s: string]: boolean} {
+		if (control.value === 'Test') {
+			return { 'invalidProjectName': true };
+		}
+		return null;
 	}
 
-	static asyncInvalidProjectName() {
-		
+	static asyncInvalidProjectName(control: FormControl): Promise<any> | Observable<any> {
+		const promise = new Promise((resolve, reject) => {
+			setTimeout(()=> {
+				if (control.value === 'Testproject') {
+					resolve({ 'invalidProjectName': true })
+				} else {
+					resolve(null);
+				}
+			}, 2000);
+		});
+
+		return promise;
 	}
 }
