1ded0a343816de15806a6687b8e4f09b89c30501
andyalam <andyalam@hotmail.com>
2017-04-30 12:12:20 -0700

    added firebase auth and auth route guards to edit/new

--- shoppingList/src/app/app.component.ts
+++ shoppingList/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature: string = 'recipes';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAiaoKISqieTCGJqEe8FNcMFxvh8yPwkhI",
+      authDomain: "ng-recipe-book-2abec.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
       this.loadedFeature = feature;
