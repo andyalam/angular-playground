9f14d40cafd65222d99aee4a94676058a066d3fa
andyalam <andyalam@hotmail.com>
2017-05-05 22:39:08 -0700

    fix of prev...

--- shoppingList/src/polyfills.ts
+++ shoppingList/src/polyfills.ts
@@ -41,8 +41,9 @@
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+//import 'core-js/es6/reflect';
+//import 'core-js/es7/reflect';
+import 'core-js'
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
@@ -53,8 +54,8 @@ import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+//import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';
 
 
 /***************************************************************************************************
